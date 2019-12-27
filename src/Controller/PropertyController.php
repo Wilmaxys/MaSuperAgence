455f83c5430ea3103c3a7a083feab90d427c81fc
Gitea <gitea@fake.local>
2019-12-27 08:56:56 +0100

    Create first pages

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+class PropertyController extends AbstractController {
+
+    public function index() : Response
+    {
+        return $this->render('property/index.html.twig');
+    }
+
+}
